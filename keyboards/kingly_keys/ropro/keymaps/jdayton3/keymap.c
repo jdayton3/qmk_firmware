e9889071726196f08a316a6472a2292b27db5a60
Jonathan Dayton <jonathandayton23@gmail.com>
2020-03-10 23:18:23 -0500

    Update ropro planck layout

--- keyboards/kingly_keys/ropro/keymaps/jdayton3/keymap.c
+++ keyboards/kingly_keys/ropro/keymaps/jdayton3/keymap.c
@@ -34,7 +34,8 @@ enum keycodes {
   DVORAK,
   PLOVER,
   BACKLIT,
-  EXT_PLV
+  EXT_PLV,
+  SHRUG
 };
 
 #define LOWER MO(_LOWER)
@@ -60,10 +61,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   	[_QWERTY] = LAYOUT(
               KC_F1,     KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,
               KC_ESC,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,      KC_0,    KC_MINUS,
-    KC_NO,    KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_BSPC,
+    KC_WHOM,  KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_BSPC,
     KC_PGUP,  KC_ESC,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      KC_SCLN,  KC_QUOT,
     KC_HOME,  KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,   KC_ENT,
-    KC_PGDN,  RGB_TOG,   KC_RCTRL, KC_LALT,  KC_LGUI,  LOWER,    KC_SPC,   KC_SPC,   RAISE,    KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT
+    KC_PGDN,  RGB_TOG,   KC_LCTRL, KC_LALT,  KC_LGUI,  LOWER,    KC_SPC,   KC_SPC,   RAISE,    KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT
   ),
 
   /* Colemak
@@ -87,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,    KC_TAB,    KC_Q,     KC_W,     KC_F,     KC_P,     KC_G,     KC_J,     KC_L,     KC_U,     KC_Y,      KC_SCLN,   KC_ESC,
     KC_PGUP,  KC_BSPC,   KC_A,     KC_R,     KC_S,     KC_T,     KC_D,     KC_H,     KC_N,     KC_E,     KC_I,      KC_O,     KC_QUOT,
     KC_HOME,  KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_K,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,   KC_ENT,
-    KC_PGDN,  RGB_TOG,   KC_RCTRL, KC_LALT,  KC_LGUI,  LOWER,    KC_SPC,   KC_SPC,   RAISE,    KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT
+    KC_PGDN,  RGB_TOG,   KC_LCTRL, KC_LALT,  KC_LGUI,  LOWER,    KC_SPC,   KC_SPC,   RAISE,    KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT
   ),
 
   /* Dvorak
@@ -111,12 +112,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,    KC_TAB,    KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,     KC_Y,     KC_F,     KC_G,     KC_C,     KC_R,      KC_L,     KC_BSPC,
     KC_PGUP,  KC_ESC,    KC_A,     KC_O,     KC_E,     KC_U,     KC_I,     KC_D,     KC_H,     KC_T,     KC_N,      KC_S,     KC_SLSH,
     KC_HOME,  KC_LSFT,   KC_SCLN,  KC_Q,     KC_J,     KC_K,     KC_X,     KC_B,     KC_M,     KC_W,     KC_V,      KC_Z,      KC_ENT,
-    KC_PGDN,  RGB_TOG,   KC_RCTRL, KC_LALT,  KC_LGUI,  LOWER,    KC_SPC,   KC_SPC,   RAISE,    KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT
+    KC_PGDN,  RGB_TOG,   KC_LCTRL, KC_LALT,  KC_LGUI,  LOWER,    KC_SPC,   KC_SPC,   RAISE,    KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT
   ),
 
   /* LOWER
    *        ,-----------------------------------------------------------------------------------.
-   *        |TOGRGB|      |      |      |Sat(-)|Hue(-)|Hue(+)|Sat(+)|      |      |Brght-|Brght+|
+   *        |RGBPLN|RGBBRT|RGBRNB|RGBSWL|Sat(-)|Hue(-)|Hue(+)|Sat(+)|RGBGRD|RGBXMS|Brght-|Brght+|
    *        |------+------+------+------+------+-------------+------+------+------+------+------|
    *        |  `   |      |      |      |      |      |      |      |      |      |      |   =  |
    *        |-----------------------------------------------------------------------------------.
@@ -130,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------------------------------------------------'
    */
     [_LOWER] = LAYOUT(
-              RGB_TOG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_SAD,  RGB_HUD,  RGB_HUI,  RGB_SAI,    KC_TRNS,    KC_TRNS,  RGB_VAD,  RGB_VAI,
+              RGB_M_P,  RGB_M_B,  RGB_M_R,  RGB_M_SW, RGB_SAD,  RGB_HUD,  RGB_HUI,  RGB_SAI,    RGB_M_G,    RGB_M_X,  RGB_VAD,  RGB_VAI,
               KC_GRAVE, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,  KC_TRNS, KC_EQUAL,
     KC_NLCK,  KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,    KC_ASTR,    KC_LPRN,  KC_RPRN,  KC_BSPC,
     KC_HOME,  KC_DEL,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_UNDS,    KC_PLUS,    KC_LCBR,  KC_RCBR,  KC_PIPE,
@@ -140,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* RAISE
    *        ,-----------------------------------------------------------------------------------.
-   *        |TOGRGB|      |      |      |Sat(-)|Hue(-)|Hue(+)|Sat(+)|      |      |Brght-|Brght+|
+   *        |RGBPLN|RGBBRT|RGBRNB|RGBSWL|Sat(-)|Hue(-)|Hue(+)|Sat(+)|RGBGRD|RGBXMS|Brght-|Brght+|
    *        |------+------+------+------+------+-------------+------+------+------+------+------|
    *        |  `   |      |      |      |      |      |      |      |      |      |      |   =  |
    *        |-----------------------------------------------------------------------------------.
@@ -154,7 +155,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------------------------------------------------'
    */
     [_RAISE] = LAYOUT(
-              RGB_TOG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_SAD,  RGB_HUD,  RGB_HUI,  RGB_SAI,  KC_TRNS,  KC_TRNS,  RGB_VAD,  RGB_VAI,
+              RGB_M_P,  RGB_M_B,  RGB_M_R,  RGB_M_SW, RGB_SAD,  RGB_HUD,  RGB_HUI,  RGB_SAI,  RGB_M_G,  RGB_M_X,  RGB_VAD,  RGB_VAI,
               KC_GRAVE, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_EQUAL,
     KC_NLCK,  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSPC,
     KC_HOME,  KC_DEL,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,
@@ -211,13 +212,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
+static bool alt_pressed = false;
+static bool ctrl_pressed = false;
+
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
+      // Cycle through windows / tabs
+      if (alt_pressed || ctrl_pressed) {
+        if (clockwise) {
+          tap_code(KC_TAB);
+        } else {
+          tap_code16(LSFT(KC_TAB));
+        }
+      } else {
         if (clockwise) {
             tap_code(KC_WH_L);
         } else {
             tap_code(KC_WH_R);
         }
+      }
     }
 }
 
@@ -231,6 +244,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+    // Layer switching
     case QWERTY:
       if (record->event.pressed) {
         set_single_persistent_default_layer(_QWERTY);
@@ -280,6 +294,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_off(_PLOVER);
       }
       return false;
+      break;
+    // Rotary Encoder behavior
+    case SHRUG:
+      if (record->event.pressed) {
+        //TODO: doesn't work, maybe because of the input mode?
+        send_unicode_hex_string("00AF 005C 005F 0028 30C4 0029 005F 002F 00AF");
+      }
+      return false;
+      break;
+    case KC_LALT:
+      alt_pressed = record->event.pressed;
+      return true;
+      break;
+    case KC_LCTL:
+      alt_pressed = record->event.pressed;
+      return true;
       break;
   }
   return true;
